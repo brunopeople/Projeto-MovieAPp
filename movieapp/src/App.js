e33d98712e650170e15cc17b06e456e3f3753ceb
bruno pessoa <brunopessoa.melo@gmail.com>
2020-09-11 20:35:13 -0300

    criando as pastas dos componentes

--- movieapp/src/App.js
+++ movieapp/src/App.js
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {MovieState} from "./Context/MovieContext.js";
+import Hero from "./components/Hero/Hero";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () =>{
+  return(
+    <MovieState>
+      <Hero/>
+    </MovieState>
   );
-}
+};
+
 
 export default App;
