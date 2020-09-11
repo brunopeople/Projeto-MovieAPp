788c2ae71085f8aa49975867c76bf7be90f01fa3
bruno pessoa <brunopessoa.melo@gmail.com>
2020-09-11 20:58:33 -0300

    instalando o @emotion/core

---
+++ movieapp/src/components/GlobalComponents/Container.js
@@ -0,0 +1,15 @@
+import {css, jsx} from "@motion/core";
+
+const Container = ({ children }) => (
+<div css={styles} className="container">
+    {children}
+</div>
+);
+
+const styles = css `
+width: 100%;
+max-width: 1300px;
+margin: 0 auto;
+`;
+
+export default Container;
