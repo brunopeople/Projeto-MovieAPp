62107e360af5c94659d835388feff81cd5c01708
bruno pessoa <brunopessoa.melo@gmail.com>
2020-09-13 19:14:37 -0300

    criando o compomente para o carregamento dos filmes ao consulta a API.

---
+++ movieapp/src/components/GlobalComponents/Loader.js
@@ -0,0 +1,54 @@
+import {css, jsx} from "@emotion/core";
+
+const Loader = () => <div css={styles} className="loader"></div>;
+
+const styles = css `
+
+position: absolute;
+top: 50%;
+left: 50%;
+transform: translate(-50%, -50%);
+width: 50px;
+height: 10px;
+background: #3498db;
+border-radius: 5px;
+animation: spinner 1.8s ease-in-out infinite;
+&:before.
+&:after{
+    position:absolute;
+    display: block;
+    content: "";
+    animation: spinner 1.8s ease-in-out infinite;
+    height: 10px;
+    border-radius: 5px;
+}
+
+&:before {
+    top: -20px;
+    left: 10px;
+    width: 40px;
+    background: #ef4836;
+}
+
+&:after {
+    bottom: -20px;
+    width: 35px;
+    backgorund: #f5ab35;
+}
+
+@keyframes spinner {
+    0% {
+        transform: translate(40px);
+    }
+
+    50% {
+        transform: translateX(-30px);
+    }
+
+    100% {
+        transform: translateX(40px);
+    }
+}
+`;
+
+export default Loader;
