db1c1c5c7724e1962b16c475bfddcf2157c00ecd
bruno pessoa <brunopessoa.melo@gmail.com>
2020-09-15 16:31:38 -0300

    adicionando o componentes movies e aplicando a lógica para que quando a consulta da API e retorna algum erro. Adicionando a animação de carregamento.

--- movieapp/src/components/Hero/HeroNav/HeroNavMenu.js
+++ movieapp/src/components/Hero/HeroNav/HeroNavMenu.js
@@ -14,4 +14,26 @@ const HeroNavMenu = () => {
     );
 };
 
+const styles = css `
+    padding-left: 80px;
+    @media(max-width: 860px){
+    position: absolute;
+    top: 0;
+    left: 0;
+    opacity: 1;
+    min-height: 100vh;
+    background: rgba(0,0,0,0.8);
+    padding-left: 0;
+    padding: 20px 50px;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    transition: all 80ms ease-in-out;
+    &.hidden{
+        left: -600px;
+        opacity: 0;
+    }
+}
+`;
 
+export default HeroNavMenu;
